8561b8ef3326a5b462d9e4d621ed8b3338a8a343
Miguel Antonio <miguel.ferreira@grupointegrado.br>
2023-08-25 21:40:56 -0300

    Finalizando tela de login

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,9 +1,27 @@
 import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
 import { Input, LoginContainer, ButtonText, LoginButton, AnimeContainer } from './styles'
 import LottieView from 'lottie-react-native';
 import catAnime from './anime.json'
+import { Alert } from 'react-native';
 
 export const Login = () => {
+  const [email, setEmail] = useState()  
+  const [password, setPassword] = useState()
+  const navigation = useNavigation()
+
+  const onHome = () => {
+    if (email === "miguel@gmail.com" && password === "1234") {
+        console.log("Entrou")
+        return navigation.navigate("HomeScreen")
+    } 
+    console.log("Erro")
+    return Alert.alert("Erro", "Login Incorreto")
+    
+    
+  }
+  console.log(email)
+  console.log(password)
   return (
     <LoginContainer>
         <AnimeContainer>
@@ -11,12 +29,16 @@ export const Login = () => {
         </AnimeContainer>
         <Input 
             placeholder='E-mail' 
+            onChangeText={setEmail}
+            value={email}
         />
         <Input 
             placeholder='Senha' 
-           secureTextEntry
+            secureTextEntry
+            onChangeText={setPassword}
+            value={password}
         />
-        <LoginButton>
+        <LoginButton onPress={onHome}>
             <ButtonText>Entrar</ButtonText>
         </LoginButton>
     </LoginContainer>
