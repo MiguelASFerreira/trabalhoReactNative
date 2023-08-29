060e695e394d703c5bfb6c80699cde320b65d8fd
Miguel Antonio <miguel.ferreira@grupointegrado.br>
2023-08-29 17:42:30 -0300

    validação do  login

--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Input, LoginContainer, ButtonText, LoginButton, AnimeContainer } from './styles'
+import { Input, LoginContainer, ButtonText, LoginButton, AnimeContainer, FormContent, ErrorMessage } from './styles'
 import LottieView from 'lottie-react-native';
 import catAnime from './anime.json'
 import { Alert } from 'react-native';
@@ -7,38 +7,62 @@ import { Alert } from 'react-native';
 export const Login = ({navigation}) => {
   const [email, setEmail] = useState()  
   const [password, setPassword] = useState()
+  const [errorEmail, setErrorEmail] = useState('')  
+  const [errorPassword, setErrorPassword] = useState('')
 
   const onHome = () => {
-    if (email === "miguel@gmail.com" && password === "1234") {
-        console.log("Entrou")
-        return navigation.navigate("HomeScreen")
-    } 
-    console.log("Erro")
-    return Alert.alert("Erro", "Login Incorreto")
-    
+    if (email === undefined && password === undefined) {
+      Alert.alert("Campos Não Preenchidos", "Preencha todos os campos")
+    } else if (email === undefined) {
+        setErrorEmail("Preencha o campo E-mail!")
+        setTimeout(() => {
+          setErrorEmail('')
+        }, 5000);
+    } else if (password === undefined) {
+        setErrorPassword("Preencha o campo Senha!")
+        setTimeout(() => {
+          setErrorPassword('')
+        }, 5000);
+    } else if (email === "Admin" && password === "1234") {
+        return navigation.navigate("Drawer")
+    } else (
+      Alert.alert("Erro", "Login Incorreto")
+    )
     
   }
   console.log(email)
   console.log(password)
+
   return (
     <LoginContainer>
-        <AnimeContainer>
+        <AnimeContainer
+          delay={600}
+          animation="bounceIn"
+        >
             <LottieView source={catAnime} loop autoPlay />
         </AnimeContainer>
-        <Input 
-            placeholder='E-mail' 
-            onChangeText={setEmail}
-            value={email}
-        />
-        <Input 
-            placeholder='Senha' 
-            secureTextEntry
-            onChangeText={setPassword}
-            value={password}
-        />
-        <LoginButton onPress={onHome}>
-            <ButtonText>Entrar</ButtonText>
-        </LoginButton>
+          <FormContent
+            delay={400}
+            animation="fadeInUpBig"
+          >
+            <Input 
+              placeholder='E-mail' 
+              onChangeText={setEmail}
+              value={email}
+              inputMode='email'
+          />
+          <ErrorMessage>{errorEmail}</ErrorMessage>
+          <Input 
+              placeholder='Senha' 
+              secureTextEntry
+              onChangeText={setPassword}
+              value={password}
+          />
+          <ErrorMessage>{errorPassword}</ErrorMessage>
+          <LoginButton onPress={onHome}>
+              <ButtonText>Entrar</ButtonText>
+          </LoginButton>
+        </FormContent>
     </LoginContainer>
   )
 }
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native'
+import * as Animatable from 'react-native-animatable'
 
 export const LoginContainer = styled.View`
     display: flex;
@@ -11,12 +12,26 @@ export const LoginContainer = styled.View`
 
 export const Input = styled.TextInput`
   width: 80%;
-  height: 40px;
+  height: 50px;
   background-color: white;
-  margin-bottom: 10px;
+  margin-bottom: 5px;
   padding: 10px;
   border-radius: 5px;
+  border: 2px solid black;
 `;
+
+export const FormContent = styled(Animatable.View)`
+  align-items: center;
+  justify-content: center;
+  border-top-right-radius: 50px;
+  border-top-left-radius: 50px;
+  background-color: white;
+  padding-top: 20px;
+  width: 100%;
+  height: 40%;
+  bottom: 0;
+  position: absolute;
+`
 
 export const LoginButton = styled.TouchableOpacity`
   background-color: #e3ca0b;
@@ -29,7 +44,13 @@ export const ButtonText = styled.Text`
   font-size: 18px;
 `;
 
-export const AnimeContainer = styled.View`
-    width: 80%;
-    height: 50%;
+export const AnimeContainer = styled(Animatable.View)`
+    width: 100%;
+    height: 40%;
+    margin-bottom: 200px;
+`
+export const ErrorMessage = styled.Text`
+  color: red;
+  font-size: 14px;
+  font-weight: bold;
 `
